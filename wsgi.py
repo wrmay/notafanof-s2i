2c0a695ae0a9031f680672a063d570355473369d
Randy May <randy@mathysphere.com>
2021-05-13 17:10:32 -0400

    change app to application - maybe thatll make s2i work

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def main():
     return "<html><body><div style=\"width:800px;font-size:x-large\"><div>Now, instead of \"having to understand Docker files\", <span style=\"font-style:italic\">I have to understand the undocumented conventions baked into many different s2i images.</span></div></body></html>"
