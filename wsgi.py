7dd8c6860bef0079ba7495e40e63dbbc4c6df976
Randy May <randy@mathysphere.com>
2021-05-13 17:32:38 -0400

    expound

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ application = Flask(__name__)
 
 @application.route("/")
 def main():
-    return "<html><body><div style=\"width:800px;font-size:x-large\"><div>Now, instead of \"having to understand Docker files\", <span style=\"font-style:italic;color:blue\">I have to understand the undocumented conventions baked into many different s2i images.</span></div></body></html>"
+    return "<html><body><div style=\"width:800px;font-size:x-large\"><div>Now, instead of \"having to understand Docker files\", <span style=\"font-style:italic;color:blue\">I have to understand the undocumented conventions baked into many different s2i images.</span></div><div>Hey, at least I learned about GitHub web hooks.</div></div></body></html>"
