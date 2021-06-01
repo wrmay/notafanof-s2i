483767460b285c987139ea84e2fbd028aca4c02b
Randy May <randy@mathysphere.com>
2021-06-01 17:17:38 -0400

    webhook testing

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ application = Flask(__name__)
 
 @application.route("/")
 def main():
-    return "<html><body><div style=\"width:800px;font-size:x-large\"><div>Webhook Test</div><div>Now, instead of \"having to understand Docker files\", <span style=\"font-style:italic;color:blue\">I have to understand the undocumented conventions baked into many different s2i images.</span></div></div></body></html>"
+    return "<html><body><div style=\"width:800px;font-size:x-large\"><div>Webhook Test 2</div><div>Now, instead of \"having to understand Docker files\", <span style=\"font-style:italic;color:blue\">I have to understand the undocumented conventions baked into many different s2i images.</span></div></div></body></html>"
